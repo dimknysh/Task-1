10b9073762eb8351b4ee3a407a7a0755cbdd0b3b
dimknysh <dimknysh@gmail.com>
2023-11-07 08:56:07 +0300

    123

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,29 +3,30 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try (var session = Util.hibernateSession()){
-//            UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
-            UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
-
-            dao.createUsersTable();
-            dao.saveUser("Dmitry", "Knysh", (byte) 32);
-            dao.saveUser("Vladimir", "Pupkin", (byte) 19);
-            dao.saveUser("Denis", "Petrov", (byte) 42);
-
+        try (var session = Util.hibernateSession()) {
+            UserServiceImpl dao = new UserServiceImpl();
 
 //            dao.createUsersTable();
+//
+//            dao.saveUser("Dmitry", "Knysh", (byte) 32);
+//            dao.saveUser("Vladimir", "Pupkin", (byte) 19);
+//            dao.saveUser("Denis", "Petrov", (byte) 42);
 //            dao.saveUser("Nikolay", "Ivanov", (byte)27);
-//            dao.saveUser("Dmitry", "Knysh", (byte)32);
-//            dao.saveUser("Vladimir", "Pupkin", (byte)19);
-//            dao.saveUser("Denis", "Petrov", (byte)42);
+
 //            System.out.println(dao.getAllUsers());
+//            for (User user : dao.getAllUsers()) {
+//                System.out.println(user.getId() + " " + user);
+//            }
+
 //            dao.cleanUsersTable();
+//
 //            dao.dropUsersTable();
         }
         // реализуйте алгоритм здесь
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,37 +1,36 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDaoJDBCImpl jdbc = new UserDaoJDBCImpl();
-    private final UserDaoHibernateImpl hibernate = new UserDaoHibernateImpl();
+//    private final UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
+    private final UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        hibernate.createUsersTable();
+        dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        hibernate.dropUsersTable();
+        dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        hibernate.saveUser(name, lastName, age);
+        dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        hibernate.removeUserById(id);
+        dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return hibernate.getAllUsers();
+        return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        hibernate.cleanUsersTable();
+        dao.cleanUsersTable();
     }
 }
